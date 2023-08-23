0c9ce66f31efeae54608bc69e0ed9b8171999be0
Denis <den.alex.chirkov@gmail.com>
2023-08-23 13:00:49 +0200

    mailer

--- js/main.js
+++ js/main.js
@@ -31,3 +31,73 @@ document.addEventListener('DOMContentLoaded', ()=>{
     overlay.addEventListener('click', closeModal);
 })
 
+document.addEventListener('DOMContentLoaded', ()=>{
+  const form = document.getElementById('form');
+  form.addEventListener('submit', formSend);
+
+  async function formSend(e){
+    e.preventDefault();
+
+    let error = formValidate(form);
+
+    let formData = new FormData(form);
+    
+
+    if(error === 0){
+      console.log(formData);
+      let response = await fetch('sendmail.php',{
+        method: 'POST',
+        body: formData
+      });
+      if(response.ok){
+        let result = await response.json();
+        alert (result.message);
+        form.reset();
+      }else{
+        alert('Error');
+      }
+    }else{
+      alert('Some required fields are empty')
+    }
+
+  }
+
+  function formValidate(form) {
+    console.log(form);
+    let error = 0;
+    let formReq = document.querySelectorAll('._req');
+    
+    for (let i=0; i<formReq.length; i++){
+      const input = formReq[i];
+      formRemoveError(input);
+      if(input.classList.contains('name')){
+        if(nameTest(input)){
+          formAddError(input);
+          error++;
+        }
+      }else{
+        if (input.value === ''){
+          formAddError(input);
+          error++;
+        }
+      }
+    }
+    return error;
+  }
+
+  function formAddError(input) {
+    // input.parentElement.classList.add('_error');
+    input.classList.add('_error');
+  }
+  function formRemoveError(input) {
+    // input.parentElement.classList.remove('_error');
+    input.classList.remove('_error');
+  }
+  function nameTest(input){
+    return !/^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/.test(input.value);
+  }
+
+
+
+
+})
